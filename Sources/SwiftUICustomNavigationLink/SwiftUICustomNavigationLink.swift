f0d6de0cc31806e5864473fcb94e0cdc3edb5102
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-07-06 14:07:44 +0200

    source added

--- Sources/SwiftUICustomNavigationLink/SwiftUICustomNavigationLink.swift
+++ Sources/SwiftUICustomNavigationLink/SwiftUICustomNavigationLink.swift
@@ -1,3 +1,27 @@
-struct SwiftUICustomNavigationLink {
-    var text = "Hello, World!"
+import SwiftUI
+
+public struct CustomNavigationLink<Content: View>: View {
+    
+    let destination: Content
+    let childView: () -> Content
+    
+    public init(destination: Content, childView: @escaping () -> Content) {
+        self.destination = destination
+        self.childView = childView
+    }
+    
+    @State private var isActive = false
+    
+    public var body: some View {
+        ZStack {
+            NavigationLink(destination: destination, isActive: self.$isActive, label: {
+                EmptyView()
+            })
+            Button(action: {
+                self.isActive = true
+            }) {
+                childView()
+            }.buttonStyle(PlainButtonStyle())
+        }
+    }
 }
